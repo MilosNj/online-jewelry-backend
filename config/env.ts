263e713d1fe90061d3c094dcc14621d94bc0f569
milos-njegovanovic-ey <milos.njegovanovic@rs.ey.com>
2022-05-31 10:21:08 +0200

    Set up env file

---
+++ config/env.ts
@@ -0,0 +1,19 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+const isProd = process.env.NODE_ENV === 'production';
+
+const parseEnv = () => {
+  const localEnv = path.resolve('.env');
+  const prodEnv = path.resolve('.env.prod');
+
+  if (!fs.existsSync(localEnv) && !fs.existsSync(prodEnv)) {
+    throw new Error('Missing environment profile');
+  }
+
+  const filePath = isProd && fs.existsSync(prodEnv) ? prodEnv : localEnv;
+
+  return { path: filePath };
+};
+
+export default parseEnv();
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
+import envConfig from '../config/env';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 49153,
-      username: 'postgres',
-      password: 'postgrespw',
-      database: 'online-jewelry',
-      autoLoadEntities: true,
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [envConfig.path],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
     }),
     ProductsModule,
     // UsersModule,
