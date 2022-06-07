2ef1a0c2f9caab1cb5c7d0f6f750ff5d70e1e8a8
milos-njegovanovic-ey <milos.njegovanovic@rs.ey.com>
2022-06-07 12:24:09 +0200

    added more space between lines

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
@@ -21,13 +22,16 @@ async function bootstrap() {
     .setVersion('1.0')
     .addTag('online-jewelry')
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
+
   const customOptions: SwaggerCustomOptions = {
     swaggerOptions: {
       persistAuthorization: true,
     },
     customSiteTitle: 'Online Jewelry API Docs',
   };
+
   SwaggerModule.setup('api', app, document, customOptions);
 
   await app.listen(4000);
