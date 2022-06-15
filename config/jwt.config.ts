a5b1e9e350c3bfd745d9654b712a2cfbc0f698d8
milos-njegovanovic-ey <milos.njegovanovic@rs.ey.com>
2022-06-15 10:15:31 +0200

    Changed expitation time to 1h

--- config/jwt.config.ts
+++ config/jwt.config.ts
@@ -6,7 +6,7 @@ export const jwtConfig: JwtModuleAsyncOptions = {
   useFactory: () => {
     return {
       secret: appConfig().appSecret,
-      signOptions: { expiresIn: '30d' },
+      signOptions: { expiresIn: 3600 },
     };
   },
 };
